d0c8701ea83b8c799509a6c84891a6807b8b4cfc
gbmikhail <vas1024@mail.ru>
2022-01-18 22:42:04 +0300

    Исправлена регистрация

--- apps/users/views.py
+++ apps/users/views.py
@@ -47,6 +47,8 @@ def registration(request):
         if register_form.is_valid():
             register_form.save()
             return HttpResponseRedirect(reverse('user:login'))
+        else:
+            messages.add_message(request, messages.ERROR, register_form.errors)
     else:
         register_form = UserRegisterForm()
 
