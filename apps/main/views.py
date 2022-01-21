a6bdafb3e0dd648012259009c9b272d137a5f987
Хлыбов Алексей Григорьевич <AGKhlybov@sberbank.ru>
2022-01-21 16:03:13 +0300

    Частичный перевод на CBV

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,14 +4,19 @@ from django.shortcuts import render
 from apps.companies.models import Company
 from apps.news.models import News
 
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-def main_view(request):
-    content = {
-        'news': News.objects.all()[:2],
-        'partners': Company.objects.filter(is_active=True)[:10],
-    }
 
-    return render(request, 'main/index.html', content)
+class HomePageList(ListView):
+    model = News
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["news"] = News.objects.all()[:2]
+        context["partners"] = Company.objects.filter(is_active=True)[:10]
+        context["title"] = "Recrupe | Главная"
+        return context
 
 
 def specification(request):
@@ -20,5 +25,3 @@ def specification(request):
 
 def posting_rules(request):
     return render(request, 'main/posting_rules.html')
-
-
--- apps/news/views.py
+++ apps/news/views.py
@@ -5,10 +5,21 @@ from apps.news.models import News
 
 class NewsListView(ListView):
     model = News
+    template_name = "news/news_list.html"
 
     def get_queryset(self):
         return News.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Список новостей"
+        return context
 
 
 class NewsDetailView(DetailView):
     model = News
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Детали новости"
+        return context
