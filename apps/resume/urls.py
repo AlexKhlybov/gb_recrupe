2f6222d09a27f55824fc5ec0751023190582575b
gbmikhail <vas1024@mail.ru>
2022-01-26 11:19:30 +0300

    Исправления ссылок

--- apps/resume/urls.py
+++ apps/resume/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
 from apps.resume import views as resume
 
@@ -6,7 +6,8 @@ app_name = 'resume'
 
 urlpatterns = [
     re_path(r'^$', resume.ResumeListView.as_view(), name='all'),
-    re_path(r'^my-resume/(?P<pk>\d+)/$', resume.MyResumeListView.as_view(), name='my-resume'),
+    # re_path(r'^my-resume/(?P<pk>\d+)/$', resume.MyResumeListView.as_view(), name='my-resume'),
+    path('my/', resume.MyResumeListView.as_view(), name='my'),
     re_path(r'^(?P<pk>\d+)/$', resume.ResumeDetailView.as_view(), name='detail'),
     
     re_path(r'^favorites/(?P<pk>\d+)/$', resume.FavoritesResumeListView.as_view(), name='favorites'),
