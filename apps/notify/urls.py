8f0244bce93049b413408b06f3e61d660758bbb6
Хлыбов Алексей Григорьевич <AGKhlybov@sberbank.ru>
2022-01-29 21:48:46 +0300

    Исправлен урл

--- apps/notify/urls.py
+++ apps/notify/urls.py
@@ -7,7 +7,7 @@ app_name = 'notify'
 
 
 urlpatterns = [
-    path('messages', notify.MessagesListView.as_view() name='massages'),
+    path('messages', notify.MessagesListView.as_view(), name='massages'),
     path('contact/', notify.contact_view, name='contact'),
     path('success/', notify.success_view, name='success'),
 ]
